83c4ce57b3e9cc8ed294b5f9f2d77a945e7699e5
Agus Sugandi <agus.sugandi95@gmail.com>
2023-02-28 20:52:35 +0700

    updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,35 +2,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../img/tarakanlogo.png'
 
 function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"  fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} className="me-3" style={{width : "35px"}}/>
+          Trakan Kota
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">HOME</Nav.Link>
-            <Nav.Link href="#pricing">ABOUT</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+          <Nav className="mx-auto">
+            <Nav.Link href="#features" className="mx-2">BERANDA</Nav.Link>
+            <Nav.Link href="#pricing" className="mx-2">PROFILE</Nav.Link>
+            <NavDropdown title="PELAYANAN" id="collasible-nav-dropdown" className="mx-2">
+              <NavDropdown.Item href="#action/3.1">Pengajuan Pengujian</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Jenis Jenis Pelayanan</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Tarif & Biaya</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-          {/* <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav> */}
+          <Nav>
+            <Nav.Link href="#deets">tarakankota@gmail.com</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,11 @@
 import CarouselBanner from "../components/Carousel"
 import thumb from '../img/thumb.webp'
+import pengujian from '../img/icon/pengujian.png'
+import tarif from '../img/icon/tarif.png'
+import steps from '../img/icon/steps.png'
+import jenislayanan from '../img/icon/jenislayanan.png'
+import example from '../img/blog/example.jpg'
+
 const Homepage = ()=>{
     return(
         <div>
@@ -14,55 +20,84 @@ const Homepage = ()=>{
                 </div>
 
                 <div className="row my-5 justify-content-center mb-5">
-                    <div className="col-4">
-                        <div className="service-card p-4">
-                           <h5 className="fw-bold">
-                            Pengajuan Uji Bahan Konstruksi
-                           </h5>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div className="service-card p-4">
-                            Tarif & Biaya
-                        </div>
-                    </div>
-                </div>
 
-                <div className="row py-5">
-                    <div className="col-12">
-                        <h5>Layanan Kami</h5>
-                        <h1 className=" fw-bold mt-2">LAB PUPR TARAKAN KOTA</h1>
-                    </div>
-                    <div className="col-4">
-                        <div className="service-card p-4 mt-4">
-                            <img src={thumb} className="w-100 rounded-4" />
-                            <h5 className="mt-3">sdrf</h5> 
-                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
+                    <div className="col-3">
+                        <div className="service-card p-4">
+                            <div className="icon-layanan p-2 mb-4 d-flex justify-content-center align-items-center">
+                                <img src={jenislayanan} className="w-75"/>
+                            </div>
+                           <h5 className="fw-bold lh-base">Jenis Jenis Pelayanan Pengujian</h5>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div className="service-card p-4 mt-4">
-                            <img src={thumb} className="w-100 rounded-4" />
-                            <h5 className="mt-3">sdrf</h5> 
-                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div className="service-card p-4 mt-4">
-                            <img src={thumb} className="w-100 rounded-4" />
-                            <h5 className="mt-3">sdrf</h5> 
-                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="row py-5">
-                    <div className="col-12 text-center">
-                        <h5>Layanan Kami</h5>
-                        <h1 className=" fw-bold mt-2">Pengajuan Tes LAB</h1>
+
+                    <div className="col-3">
+                        <div className="service-card p-4">
+                            <div className="icon-layanan p-2 mb-4 d-flex justify-content-center align-items-center">
+                                <img src={pengujian} className="w-75"/>
+                            </div>
+                           <h5 className="fw-bold lh-base">Pengajuan Uji Bahan Konstruksi</h5>
+                        </div>
+                    </div>
+
+                    <div className="col-3">
+                        <div className="service-card p-4">
+                            <div className="icon-layanan p-2 mb-4 d-flex justify-content-center align-items-center">
+                                <img src={tarif} className="w-75"/>
+                            </div>
+                           <h5 className="fw-bold lh-base">Tarif & Biaya Pengujian Bahan Konstruksi</h5>
+                        </div>
+                    </div>
+
+                    <div className="col-3">
+                        <div className="service-card p-4">
+                            <div className="icon-layanan p-2 mb-4 d-flex justify-content-center align-items-center">
+                                <img src={steps} className="w-75"/>
+                            </div>
+                           <h5 className="fw-bold lh-base">Mekanisme Penujian Bahan Konstruksi</h5>
+                        </div>
                     </div>
                 </div>
                 
+                <div className="row mt-5">
+                    <div className="col-12">
+                        <h5 className="text-blue">Lab PUPR Trakan Kota</h5>
+                        <h1 className=" fw-bold mt-2">Tugas Pokok dan Fungsi</h1>
+                    </div>
+                </div>
+
+                <div className="row mt-5">
+                    <div className="col-12 mb-5">
+                        <h5 className="text-blue">Lab PUPR Trakan Kota</h5>
+                        <h1 className=" fw-bold mt-2">Program & Kegiatan Terkini</h1>
+                    </div>
+
+                    <div className="col-4">
+                        <div className="service-card p-4">
+                            <img src={example} className="ratio ratio-4x3 w-100 rounded-4" />
+                            <h5 className="mt-3">sdrf</h5> 
+                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="service-card p-4">
+                            <img src={example} className="ratio ratio-4x3 w-100 rounded-4" />
+                            <h5 className="mt-3">sdrf</h5> 
+                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="service-card p-4">
+                            <img src={example} className="ratio ratio-4x3 w-100 rounded-4" />
+                            <h5 className="mt-3">sdrf</h5> 
+                            <span>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus.  </span>
+                        </div>
+                    </div>
+
+                    <div className="col-12 text-center">
+                        <a href="" className="text-decoration-none mx-auto d-block mt-5 link">Lihat Lainnya...</a>
+                    </div>
+                </div>
 
             </div>
 
